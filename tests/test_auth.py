aa222d590e1865b00424fb1ddbbfd42f7015ba60
Matin Tamizi <matin@cuenca.com>
2018-11-12 02:10:22 -0200

    easier interface to implement

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-import arcus
+from arcus import Client
 from arcus.exc import InvalidAuth
 
 
@@ -11,8 +11,8 @@ SECRET_KEY = os.environ['ARCUS_SECRET_KEY']
 
 
 def test_valid_auth():
-    arcus.configure(API_KEY, SECRET_KEY, sandbox=True)
-    resp = arcus.get('/account')
+    client = Client(API_KEY, SECRET_KEY, sandbox=True)
+    resp = client.get('account')
     assert resp.status_code == 200
     assert resp.data['currency'] == 'MXN'
     assert isinstance(resp.data['account_balance'], float)
@@ -20,12 +20,8 @@ def test_valid_auth():
 
 
 def test_invalid_auth():
-    with pytest.raises(InvalidAuth) as excinfo:
-        arcus.get('/account')
-    assert excinfo.value == 'API credentials have not been configured'
-
     # Attempt using sandbox creds on production API endpoint
-    arcus.configure(API_KEY, SECRET_KEY)  # default is sandbox=False
+    client = Client(API_KEY, SECRET_KEY)  # default is sandbox=False
     with pytest.raises(InvalidAuth) as excinfo:
-        arcus.get('/account')
+        client.get('account')
     assert excinfo.value == 'Invalid API authentication credentials'
