4d21cad09245159ab41d32dbfd109eb5eb324a1e
Matin Tamizi <matin@cuenca.com>
2018-11-13 13:35:49 -0600

    type hinting

--- arcus/client.py
+++ arcus/client.py
@@ -1,5 +1,5 @@
-import requests
 import json
+import requests
 
 from arcus import authtools
 from arcus.exc import InvalidAuth
@@ -41,34 +41,30 @@ class Client:
                 **kwargs) -> dict:
         url = self.base_url + endpoint
         headers = self._build_headers(endpoint, data)
-
         response = requests.request(
             method, url, headers=headers, json=data, **kwargs)
-
         if response.status_code == 401:
             raise InvalidAuth('Invalid API authentication credentials')
-
         return response.json()
 
-    def _build_headers(self, endpoint, data):
-        headers = [
-            ('Accept', f'application/vnd.regalii.v{self.api_version}+json')
-        ]
+    def _build_headers(self, endpoint: str, data: dict) -> dict:
+        headers = [('Accept',
+                    f'application/vnd.regalii.v{self.api_version}+json')]
         headers.append(self._compute_md5_header(data))
         headers.append(self._compute_date_header())
         headers.append(self._compute_auth_header(headers, endpoint))
         return dict(headers)
 
     @staticmethod
-    def _compute_md5_header(data):
+    def _compute_md5_header(data: dict) -> tuple:
         data = json.dumps(data)
         return 'Content-MD5', authtools.get_md5(data)
 
     @staticmethod
-    def _compute_date_header():
+    def _compute_date_header() -> tuple:
         return 'Date', authtools.get_timestamp()
 
-    def _compute_auth_header(self, headers, endpoint):
+    def _compute_auth_header(self, headers: list, endpoint: str) -> tuple:
         content_type = CONTENT_TYPE
         headers = dict(headers)
         content_md5 = headers['Content-MD5']
