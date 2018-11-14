f1e687df308e3557fd62ca9b8947fb20356e8020
Felipe López <flh.1989@gmail.com>
2018-11-14 12:49:47 -0600

    fix asserts

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -41,7 +41,8 @@ def test_compute_auth_header():
         ('Content-MD5', ''),
         ('Date', 'Wed, 02 Nov 2016 17:26:52 GMT')
     ]
-    auth_header = compute_auth_header(headers, endpoint, API_KEY, SECRET_KEY)
-    assert type(auth_header) is tuple
-    assert auth_header[0] == 'Authorization'
-    assert auth_header[1] == f'APIAuth {API_KEY}:{CHECKSUM}'
+    header_key, header_value = compute_auth_header(headers,
+                                                   endpoint,
+                                                   API_KEY, SECRET_KEY)
+    assert header_key == 'Authorization'
+    assert header_value == f'APIAuth {API_KEY}:{CHECKSUM}'
