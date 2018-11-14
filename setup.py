341414515429e21226e4ef34bc5866b418b10d29
Matin Tamizi <matin@cuenca.com>
2018-11-14 13:01:23 -0600

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='arcus',
-    version='0.0.3',
+    version='0.0.5',
     author='Cuenca',
     author_email='dev@cuenca.com',
     description='Arcus API Client',
