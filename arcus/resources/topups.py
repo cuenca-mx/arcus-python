0d2787f8391367299268e31bb1f665a5193e1eed
Felipe López <flh.1989@gmail.com>
2018-11-26 20:35:02 -0600

    comments on why using v1.6 for top-ups

--- arcus/resources/topups.py
+++ arcus/resources/topups.py
@@ -1,6 +1,10 @@
 from .base import Resource
 
-
+"""
+Arcus API version 1.6 should be used in order to make top-up operations. 
+It's because newer API versions don't support this kind of operations.
+https://www.arcusfi.com/api/v1_6/#pay
+"""
 TOPUP_API_VERSION = '1.6'
 
 
