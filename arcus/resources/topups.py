0f1ed4147857a4db42ec5f0a19664fc983433f25
Matin Tamizi <matin@cuenca.com>
2018-11-27 10:48:51 -0600

    make currency an optional parameter

--- arcus/resources/topups.py
+++ arcus/resources/topups.py
@@ -12,11 +12,17 @@ TOPUP_API_VERSION = '1.6'
 class Topup(Resource):
 
     @classmethod
-    def create(cls, biller_id: int, account_number: str, amount: float):
+    def create(
+            cls,
+            biller_id: int,
+            account_number: str,
+            amount: float,
+            currency: str = 'MXN'
+    ):
         data = dict(biller_id=biller_id,
                     account_number=account_number,
                     amount=amount,
-                    currency='MXN')
+                    currency=currency)
         topup_dict = cls._client.post('/bill/pay',
                                       data,
                                       api_version=TOPUP_API_VERSION)
