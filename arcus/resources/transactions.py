6034e02a07f85a680a0a298a2965e9d79a33d8b0
Matin Tamizi <matin@cuenca.com>
2018-12-28 12:44:22 -0600

    Transaction type hinting

--- arcus/resources/transactions.py
+++ arcus/resources/transactions.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from .base import Resource
 
 
@@ -5,14 +7,14 @@ class Transaction(Resource):
     _endpoint = '/transactions'
 
     @classmethod
-    def cancel(cls, transaction_id: int):
+    def cancel(cls, transaction_id: Union[int, str]):
         transaction_data = dict(id=transaction_id)
-        transaction_dict = cls._client.post('/transaction/cancel',
-                                            transaction_data)
+        transaction_dict = cls._client.post(
+            '/transaction/cancel', transaction_data)
         return Transaction(**transaction_dict)
 
     @classmethod
-    def get(cls, transaction_id):
+    def get(cls, transaction_id: Union[int, str]):
         transaction_dict = (
             cls._client.get(f'{cls._endpoint}?q[id_eq]={transaction_id}'))
         return Transaction(**transaction_dict['transactions'][0])
