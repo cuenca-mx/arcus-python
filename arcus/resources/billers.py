93cbdc03e27fdab8f7f2ab3d20ddacb2025b2563
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-01-10 17:08:56 -0600

    Apply style codes

--- arcus/resources/billers.py
+++ arcus/resources/billers.py
@@ -1,4 +1,4 @@
-from pydash import filter_, union
+from pydash import filter_
 
 from .base import Resource
 
@@ -11,18 +11,18 @@ class Biller(Resource):
     def list(cls, **kwargs):
         billers_list = []
         for endpoint in ENDPOINTS:
-            billers_list = union(billers_list, cls.list_billers(endpoint))
+            billers_list.extend(cls.list_billers(endpoint))
         billers = [Biller(**biller_dict)
                    for biller_dict in filter_(billers_list, kwargs)]
         return billers
 
     @classmethod
-    def list_billers(cls, endpoint):
+    def list_billers(cls, endpoint: str) -> list:
         page = 1
         billers = []
         while True:
             resp = cls._client.get(f'/billers/{endpoint}?page={page}')
-            billers = union(billers, resp['billers'])
+            billers.extend(resp['billers'])
             if len(resp['billers']) == 0:
                 break
             page += 1
