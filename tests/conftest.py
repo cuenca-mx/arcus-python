c20c16bb96521a7d51aae3fd02a77f450ce7d34a
Gleekzone <46584253+Gleekzone@users.noreply.github.com>
2019-02-13 17:20:04 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,13 @@
 import pytest
 
-from botmaker import Client
+from arcus import Client
 
 
 @pytest.fixture(scope='module')
 def vcr_config():
     config = dict()
     # Replace the Authorization request header with 'DUMMY' in cassettes
-    config['filter_headers'] = [('access-token', 'DUMMY')]
-    config['filter_post_data_parameters'] = [
-        ('chatChannelNumber', '5215500000000'),
-        ('platformContactId', '5215522222222'),
-        ('contacts', ['+55 1 55 1234 5678'])
-    ]
+    config['filter_headers'] = [('Authorization:', 'DUMMY')]
     return config
 
 
@@ -20,3 +15,4 @@ def vcr_config():
 def client():
     # Using BOTMAKER_ACCESS_TOKEN from env
     yield Client()
+
