65f700a48e47acac7d5881d1cb1370ffb53b43c5
Gleekzone <46584253+Gleekzone@users.noreply.github.com>
2019-02-13 18:05:58 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ def vcr_config():
     config = dict()
     # Replace the Authorization request header with 'DUMMY' in cassettes
     config['filter_headers'] = [('Authorization:', 'DUMMY')]
-    config['filter_auth_post_parameters'] = [
+    config['filter_auth_parameters'] = [
         ('biller_id', '40'),
         ('account_number', '501000000007')
     ]
