e191f838d55d1273b229693f98d9d01bb7724d77
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-08-05 13:04:41 -0500

    add test

--- tests/resources/test_accounts.py
+++ tests/resources/test_accounts.py
@@ -1,5 +1,6 @@
 import pytest
 
+from arcus.exc import Forbidden
 from arcus.resources import Account
 
 
@@ -19,3 +20,9 @@ def test_get_account_info_proxy(client_proxy):
     assert account.currency == 'MXN'
     assert type(account.balance) is float
     assert account.balance > account.minimum_balance
+
+
+@pytest.mark.vcr
+def test_post_method_proxy(client_proxy):
+    with pytest.raises(Forbidden):
+        client_proxy.post('/account', {})
