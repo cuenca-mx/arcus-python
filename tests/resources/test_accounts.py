bb2b4f01560d0f3afde96c4058530dc0183c75a7
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-08-19 15:27:57 -0500

    add test for proxy

--- tests/resources/test_accounts.py
+++ tests/resources/test_accounts.py
@@ -16,16 +16,17 @@ def test_get_account_info(client):
         assert account.balance > account.minimum_balance
 
 
-@pytest.mark.skip
 def test_get_account_info_proxy(client_proxy):
-    account = client_proxy.account
-    assert type(account) is Account
-    assert account.currency == 'MXN'
-    assert type(account.balance) is float
-    assert account.balance > account.minimum_balance
+    accounts = client_proxy.accounts
+    assert type(accounts) is dict
+    assert set(accounts.keys()) == {'primary'}
+    for account in accounts.values():
+        assert type(account) is Account
+        assert account.currency == 'MXN'
+        assert type(account.balance) is float
+        assert account.balance > account.minimum_balance
 
 
-@pytest.mark.skip
 def test_post_method_proxy(client_proxy):
     with pytest.raises(Forbidden):
         client_proxy.post('/account', {})
