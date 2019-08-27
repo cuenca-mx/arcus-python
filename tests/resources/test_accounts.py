1f181c8f09900adf5240a88b2c6236804d6698b3
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-08-27 10:24:51 -0500

    mock ARCUS_PROXY

--- tests/resources/test_accounts.py
+++ tests/resources/test_accounts.py
@@ -1,5 +1,7 @@
 import pytest
+from unittest.mock import patch
 
+from arcus import Client
 from arcus.exc import Forbidden
 from arcus.resources import Account
 
@@ -30,3 +32,16 @@ def test_get_account_info_proxy(client_proxy):
 def test_post_method_proxy(client_proxy):
     with pytest.raises(Forbidden):
         client_proxy.post('/account', {})
+
+
+@patch.dict('os.environ', {'ARCUS_PROXY': ''})
+def test_get_account_info_without_proxy():
+    client_without_proxy = Client(sandbox=True)
+    accounts = client_without_proxy.accounts
+    assert type(accounts) is dict
+    assert set(accounts.keys()) == {'primary', 'topup'}
+    for account in accounts.values():
+        assert type(account) is Account
+        assert account.currency == 'MXN'
+        assert type(account.balance) is float
+        assert account.balance > account.minimum_balance
