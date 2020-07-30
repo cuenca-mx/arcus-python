28af1d638fe3fedd807f56b2da2bc54ad95d4a28
cuencandres <57640881+cuencandres@users.noreply.github.com>
2020-07-30 15:36:02 -0500

    Better Testing (#86)
    * better testing
    
    * dataclasses
    
    * complete hours
    
    * changes

--- setup.py
+++ setup.py
@@ -4,16 +4,6 @@ from setuptools import find_packages, setup
 
 version = SourceFileLoader('version', 'arcus/version.py').load_module()
 
-test_requires = [
-    'pytest==5.4.3',
-    'pytest-vcr==1.0.2',
-    'pytest-cov==2.10.0',
-    'black==19.10b0',
-    'isort==5.0.*',
-    'flake8==3.8.1',
-    'mypy==0.782',
-    'requests-mock==1.8.0',
-]
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -30,15 +20,12 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[
-        'requests==2.21.0',
-        'pytz==2020.1',
-        'iso8601==0.1.12',
-        'pydash==4.7.4',
+        'requests>=2.21,<=2.22',
+        'pytz>=2020.1,<=2020.2',
+        'iso8601>=0.1,<=0.2',
+        'pydash>=4.7,<=4.8',
         'dataclasses>=0.6;python_version<"3.7"',
     ],
-    setup_requires=['pytest-runner'],
-    tests_require=test_requires,
-    extras_require=dict(test=test_requires),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
